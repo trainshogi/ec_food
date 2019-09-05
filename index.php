ad879d51ee77644b4bc55d1209bb1b1aaa4b2f05
itonao <itokina110700815@gmail.com>
2019-09-05 12:34:40 +0900

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/indexstyle.css">
 </head>
-<body style="margin-left: auto; margin-right: auto;">
-	<div id="header">
+<body style="margin-left: auto; margin-right: auto;" class="body";>
+	<div id="header" class="header">
 		<a href="index.html"><img src="img/logo.png" alt="Rakuten, Inc."></a>
 		<div class="header-title"><h1>レシピで検索</h1></div>
 	</div>
@@ -18,13 +18,41 @@
     	<input type="search" name="search" placeholder="キーワードを入力してください">
     	<input type="submit" name="submit" value="検索">
 	</div>
-	<div style="text-align: center;">
+		<table class="table" style="table-layout:fixed;" align="center">
+	    <thead>
+	        <tr bgcolor=#f5deb3>
+	            <th style="width: 60px;"><img src="img/cook.png" alt="cook" class="cookphoto"></th>
+	            <th style="width:100px;">料理のリスト</th>
+	        </tr>
+	    </thead>
+	    <tbody bgcolor=#fffacd>
+	        <tr>
+	            <td><a href="#"><img src="img/meatpotato.jpg" alt="meatpotato" class="cookphoto"></td>
+	            <td><a href="#">肉じゃが</td>
+	        </tr>
+	        <tr>
+	            <td><a href="#"><img src="img/curry.jpg" alt="curry" class="cookphoto"></td>
+	            <td><a href="#">カレー</td>
+	        </tr>
+	        <tr>
+	            <td><a href="#"><img src="img/stew.jpg" alt="stew" class="cookphoto"></td>
+	            <td><a href="#">シチュー</td>
+	        </tr>
+	    </tbody>
+</table>
+
+
+
+
+
+
+	<!--<div style="text-align: center;">
 		<div class="list-group recipi_list">
 			<a href="#" class="list-group-item list-group-item-action active">料理のリスト</a>
 			<a href="#" class="list-group-item list-group-item-action">肉じゃが</a>
 			<a href="#" class="list-group-item list-group-item-action">カレー</a>
 			<a href="#" class="list-group-item list-group-item-action">シチュー</a>
 		</div>
-	</div>
+	</div>-->
 </body>
 </html>
