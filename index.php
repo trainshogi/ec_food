881acd1d89d5c5813a6298eb6c758a2012a87a37
itonao <itokina110700815@gmail.com>
2019-09-12 02:01:11 +0900

    Update index.php

--- index.php
+++ index.php
@@ -27,27 +27,27 @@
 	    <tbody bgcolor=#ffffff>
 	        <tr>
 	            <td class="pic"><a href="result.html?name=%E8%82%89%E3%81%98%E3%82%83%E3%81%8C"><img src="img/meatpotato.jpg" alt="meatpotato" class="cookphoto"></td>
-	            <td class="cname"><a href="#">肉じゃが</td>
-	            <td><a href="#"><img src="img/meatpotato.jpg" alt="meatpotato" class="cookphoto"></td>
-	            <td><a href="#">肉じゃが</td>
-	            <td><a href="#"><img src="img/meatpotato.jpg" alt="meatpotato" class="cookphoto"></td>
-	            <td><a href="#">肉じゃが</td>
+	            <td class="cname"><a href="result.html?name=%E8%82%89%E3%81%98%E3%82%83%E3%81%8C">肉じゃが</td>
+	            <td><a href="result.html?name=%E9%BA%BB%E5%A9%86%E8%B1%86%E8%85%90"><img src="img/tohu.jpg" alt="mabodohu" class="cookphoto"></td>
+	            <td><a href="result.html?name=%E9%BA%BB%E5%A9%86%E8%B1%86%E8%85%90">麻婆豆腐</td>
+	            <td><a href="result.html?name=%E3%82%AB%E3%83%AB%E3%83%9C%E3%83%8A%E3%83%BC%E3%83%A9"><img src="img/karubonara.jpg" alt="karubonara" class="cookphoto"></td>
+	            <td><a href="result.html?name=%E3%82%AB%E3%83%AB%E3%83%9C%E3%83%8A%E3%83%BC%E3%83%A9">カルボナーラ</td>
 	        </tr>
 	        <tr>
-	            <td><a href="#"><img src="img/curry.jpg" alt="curry" class="cookphoto"></td>
-	            <td><a href="#">カレー</td>
-	            <td><a href="#"><img src="img/meatpotato.jpg" alt="meatpotato" class="cookphoto"></td>
-	            <td><a href="#">肉じゃが</td>
-	            <td><a href="#"><img src="img/meatpotato.jpg" alt="meatpotato" class="cookphoto"></td>
-	            <td><a href="#">肉じゃが</td>
+	            <td><a href="result.html?name=%E3%82%AB%E3%83%AC%E3%83%BCC"><img src="img/curry.jpg" alt="curry" class="cookphoto"></td>
+	            <td><a href="result.html?name=%E3%82%AB%E3%83%AC%E3%83%BC">カレー</td>
+	            <td><a href="result.html?name=%E8%A6%AA%E5%AD%90%E4%B8%BC"><img src="img/oyakodon.jpg" alt="oyakodon" class="cookphoto"></td>
+	            <td><a href="result.html?name=%E8%A6%AA%E5%AD%90%E4%B8%BC">親子丼</td>
+	            <td><a href="result.html?name=%E3%82%B1%E3%83%BC%E3%82%AD"><img src="img/cake.jpg" alt="cake" class="cookphoto"></td>
+	            <td><a href="result.html?name=%E3%82%B1%E3%83%BC%E3%82%AD">ケーキ</td>
 	        </tr>
 	        <tr>
-	            <td><a href="#"><img src="img/stew.jpg" alt="stew" class="cookphoto"></td>
-	            <td><a href="#">シチュー</td>
-	            <td><a href="#"><img src="img/meatpotato.jpg" alt="meatpotato" class="cookphoto"></td>
-	            <td><a href="#">肉じゃが</td>
-	            <td><a href="#"><img src="img/meatpotato.jpg" alt="meatpotato" class="cookphoto"></td>
-	            <td><a href="#">肉じゃが</td>
+	            <td><a href="result.html?name=%E3%82%B7%E3%83%81%E3%83%A5%E3%83%BC"><img src="img/stew.jpg" alt="stew" class="cookphoto"></td>
+	            <td><a href="result.html?name=%E3%82%B7%E3%83%81%E3%83%A5%E3%83%BC">シチュー</td>
+	            <td><a href="result.html?name=%E5%A4%A9%E6%B4%A5%E9%A3%AF"><img src="img/tensinhan.jpg" alt="tensinhan" class="cookphoto"></td>
+	            <td><a href="result.html?name=%E5%A4%A9%E6%B4%A5%E9%A3%AF">天津飯</td>
+	            <td><a href="result.html?name=%E3%82%BF%E3%83%94%E3%82%AA%E3%82%AB"><img src="img/tapioka.jpg" alt="tapioka" class="cookphoto"></td>
+	            <td><a href="result.html?name=%E3%82%BF%E3%83%94%E3%82%AA%E3%82%AB">タピオカ</td>
 	        </tr>
 	    </tbody>
 	</table>
