802b9eae09cc8cb9a3c533d92be707acf71e3b68
trainshogi <densyashogi@gmail.com>
2019-09-12 11:07:58 +0800

    買えた
    Buy Nuowで買えるよ

--- php/api.php
+++ php/api.php
@@ -58,8 +58,8 @@ function get_ingredients($query){
     return $json;
 }
 
-// echo get_ingredients($_POST['cook_name']);
+echo get_ingredients($_POST['cook_name']);
 // echo get_ingredients('https://recipe.rakuten.co.jp/recipe/1460015382/');
-//echo get_ingredients('肉じゃが');
-echo get_shopids("ショップ１");
+// echo get_ingredients('肉じゃが');
+// echo get_shopids("ショップ１");
 exit();
